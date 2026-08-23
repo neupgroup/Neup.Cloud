2e398431106fca240949d0045801296a10b7ea17
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2026-08-24 01:13:30 +0545

    Updated the server homepage.

--- app/(main)/server/commands/history/page.tsx
+++ app/(main)/server/commands/history/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 import type { Metadata } from 'next';
-import { CommandsContent } from '../page';
+import { CommandsContent } from '../commands-content';
 
 export const metadata: Metadata = {
   title: 'Command History, Neup.Cloud',
